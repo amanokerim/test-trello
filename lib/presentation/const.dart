5c5219fc1360c91380ff0add46723242b9e5af1a
amanokerim <amanokerim@gmail.com>
2021-02-13 18:24:01 +0500

    implement login form validation

--- lib/presentation/const.dart
+++ lib/presentation/const.dart
@@ -3,15 +3,20 @@ import 'package:flutter/material.dart';
 const Color myPrimaryColor = Color(0xff89FDDD);
 
 InputDecoration myLoginDecoration = InputDecoration(
-  
   focusedBorder: OutlineInputBorder(
     borderSide: BorderSide(color: myPrimaryColor),
     borderRadius: BorderRadius.circular(30.0),
   ),
   enabledBorder: OutlineInputBorder(
-    borderSide: BorderSide(
-      color: Colors.grey,
-    ),
+    borderSide: BorderSide(color: Colors.grey),
+    borderRadius: BorderRadius.circular(30.0),
+  ),
+  focusedErrorBorder: OutlineInputBorder(
+    borderSide: BorderSide(color: Colors.red),
+    borderRadius: BorderRadius.circular(30.0),
+  ),
+  errorBorder: OutlineInputBorder(
+    borderSide: BorderSide(color: Colors.red),
     borderRadius: BorderRadius.circular(30.0),
   ),
   contentPadding: const EdgeInsets.symmetric(horizontal: 20.0),
--- lib/presentation/screens/login_screen.dart
+++ lib/presentation/screens/login_screen.dart
@@ -1,7 +1,17 @@
 import 'package:flutter/material.dart';
 import 'package:testtrello/presentation/const.dart';
 
-class LoginScreen extends StatelessWidget {
+class LoginScreen extends StatefulWidget {
+  @override
+  _LoginScreenState createState() => _LoginScreenState();
+}
+
+class _LoginScreenState extends State<LoginScreen> {
+  final _usernameKey = GlobalKey<FormFieldState>();
+  final _passwordKey = GlobalKey<FormFieldState>();
+
+  String _username = "", _password = "";
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -14,38 +24,65 @@ class LoginScreen extends StatelessWidget {
             child: Column(
           children: [
             SizedBox(height: 30.0),
-            TextFormField(
-              textAlign: TextAlign.center,
-              decoration:
-                  myLoginDecoration.copyWith(hintText: "Enter your username"),
-              cursorColor: myPrimaryColor,
-            ),
+            _buildUsernameTextFormField(),
             SizedBox(height: 20.0),
-            TextFormField(
-              textAlign: TextAlign.center,
-              decoration:
-                  myLoginDecoration.copyWith(hintText: "Enter your password"),
-              cursorColor: myPrimaryColor,
-            ),
+            _buildPasswordTextFormField(),
             SizedBox(height: 20.0),
-            Container(
-              padding: EdgeInsets.symmetric(vertical: 16.0),
-              decoration: BoxDecoration(
-                color: myPrimaryColor,
-                borderRadius: BorderRadius.circular(30.0),
-              ),
-              child: Center(
-                  child: Text(
-                "Log in",
-                style: Theme.of(context)
-                    .textTheme
-                    .button
-                    .copyWith(color: Colors.black),
-              )),
-            )
+            _buildLoginButton(context)
           ],
         )),
       ),
+    );
+  }
+
+  Container _buildLoginButton(BuildContext context) {
+    return Container(
+      padding: EdgeInsets.symmetric(vertical: 16.0),
+      decoration: BoxDecoration(
+        color: myPrimaryColor,
+        borderRadius: BorderRadius.circular(30.0),
+      ),
+      child: Center(
+          child: Text(
+        "Log in",
+        style: Theme.of(context).textTheme.button.copyWith(color: Colors.black),
+      )),
+    );
+  }
+
+  TextFormField _buildPasswordTextFormField() {
+    return TextFormField(
+      key: _passwordKey,
+      obscureText: true,
+      textAlign: TextAlign.center,
+      decoration: myLoginDecoration.copyWith(hintText: "Enter your password"),
+      cursorColor: myPrimaryColor,
+      validator: (v) {
+        if (_password.length < 8) return "Minimum is 8 characters";
+        return null;
+      },
+      onChanged: (v) {
+        setState(() => _password = v);
+        _passwordKey.currentState.validate();
+      },
+    );
+  }
+
+  TextFormField _buildUsernameTextFormField() {
+    return TextFormField(
+      key: _usernameKey,
+      autofocus: true,
+      textAlign: TextAlign.center,
+      decoration: myLoginDecoration.copyWith(hintText: "Enter your username"),
+      cursorColor: myPrimaryColor,
+      validator: (v) {
+        if (_username.length < 4) return "Minimum is 4 characters";
+        return null;
+      },
+      onChanged: (v) {
+        setState(() => _username = v);
+        _usernameKey.currentState.validate();
+      },
     );
   }
 }
